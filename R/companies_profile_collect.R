337133e8d3d590566204bd29462a6c202cfb6728
Joao <joaocunha11@hotmail.com>
2018-09-19 10:47:33 +0100

    [companieshr] - adding verbose functionality

--- R/companies_profile_collect.R
+++ R/companies_profile_collect.R
@@ -6,11 +6,12 @@
 #'
 #' All Companies House APIs are accessed by company number. The user can collect company numbers from monthly extracts offered by CompanyHouse that can be accessed \href{http://download.companieshouse.gov.uk/en_output.html}{here}.
 #'
-#' @param companies_df A data-frame with CompanyNumber column (see companies data file as example).
+#' @param companies A data-frame with CompanyNumber column (see companies data file as example) or a character vector with CompanyNumber.
 #' @param auth_api_key The CompaniesHouse API Key. To generate one please go to \href{https://developer.companieshouse.gov.uk/api/docs/index/gettingStarted/apikey_authorisation.html}{CompaniesHouse Developers Page}.
 #' @param time_to_rest In case the API repsonse is 429 ('Too Many Requests') the user can select the period to wait until it tries again. It defaults to 3 seconds.
 #' @param items_per_page The items per page request to try and return (defaults to 1000).
-#' @param join A boolean value that when TRUE whill return the results joined with the original df parsed. Defaults to TRUE.
+#' @param join A boolean value that when TRUE whill return the results joined with the original df parsed. It only applies when companies argument is a data.frame. Defaults to TRUE.
+#' @param verbose Boolean value set to TRUE to print the status of each API call.
 #'
 #' @return A list with two elements:
 #' \itemize{
@@ -22,23 +23,23 @@
 #'
 #' @examples
 #' \dontrun{
-#' companies_api_collect(companies_df, auth_api_key , time_to_rest = 3)
+#' companies_api_collect(companies, auth_api_key , time_to_rest = 3)
 #'}
 
 #To write:
 
 #A general errorHandling function to be called in all the API endpoint wrappers (argument controls)
 
-companies_profile_collect = function(companies_df, auth_api_key, time_to_rest = 3, items_per_page = 1000, join = TRUE ){
+companies_profile_collect = function(companies, auth_api_key, time_to_rest = 3, items_per_page = 1000, join = TRUE, verbose = TRUE){
 
   url_f = "https://api.companieshouse.gov.uk"
   final_df = list()
   errorLogs = list()
   auth_keys = auth_api_key
 
-  if("data.frame" %in% class(companies_df)){
-      companies_numbers = unique(companies_df$CompanyNumber)[!is.na(unique(companies_df$CompanyNumber))]
-  }else if(class(companies_df) %in% c("character", "numeric")){companies_numbers = as.character(unique(companies_df[!is.na(companies_df)]))}
+  if("data.frame" %in% class(companies)){
+      companies_numbers = unique(companies$CompanyNumber)[!is.na(unique(companies$CompanyNumber))]
+  }else if(class(companies) %in% c("character", "numeric")){companies_numbers = as.character(unique(companies[!is.na(companies)]))}
 
   API_responses = api_codes_lookup()
 
@@ -51,7 +52,7 @@ companies_profile_collect = function(companies_df, auth_api_key, time_to_rest = 
     path_f = sprintf("/company/%s/?items_per_page=%s", companies_numbers[i], items_per_page)
     results_all <- httr::GET(url = url_f, path = path_f, httr::add_headers(Host = "api.companieshouse.gov.uk", Authorization = auth_keys))
 
-    if(i == 1){cat(paste0("Current Limit: ", results_all$headers$`x-ratelimit-remain`, " requests\n"))}
+    if(i == 1){cat(paste0("Collecting Companies House Data (Current API Limit: ", results_all$headers$`x-ratelimit-remain`, " requests)...\n"))}
 
     # if(httr::http_type(results_all) != "application/json"){next("API did not return json", call. = FALSE)}
     if(results_all$status_code == 200){
@@ -63,7 +64,7 @@ companies_profile_collect = function(companies_df, auth_api_key, time_to_rest = 
                                     CompanyName = company_name) %>%
                       dplyr::select(CompanyNumber, CompanyName, dplyr::everything())
 
-      cat(paste0(nrow(final_df[[i]]), " Company Profile Information record for CompanyNumber  ", companies_numbers[i], " was/were colected successfully \n"))
+      if(verbose){cat(paste0(nrow(final_df[[i]]), " Company Profile Information record for CompanyNumber  ", companies_numbers[i], " was/were colected successfully \n"))}
 
       i = i + 1
       next()
@@ -71,12 +72,12 @@ companies_profile_collect = function(companies_df, auth_api_key, time_to_rest = 
     }else if(results_all$status_code == 429){
 
       Sys.sleep(time_to_rest)
-      cat(paste0("\nToo many requests. ", time_to_rest, " seconds until next try ...\n"))
+      if(verbose){cat(paste0("\nToo many requests. ", time_to_rest, " seconds until next try ...\n"))}
       next()
 
     }else{
 
-      cat(paste0("\nAn error occured with an API Response code: ", results_all$status_code, ". Skipping CompanyID ", companies_numbers[i], ".\n"))
+      if(verbose){cat(paste0("\nAn error occured with an API Response code: ", results_all$status_code, ". Skipping CompanyID ", companies_numbers[i], ".\n"))}
 
 
       error_log = data.frame(ResponseCode = results_all$status_code,
@@ -94,9 +95,9 @@ companies_profile_collect = function(companies_df, auth_api_key, time_to_rest = 
   }
 
   #formatting the final results:
-  if(join & ("data.frame" %in% class(companies_df))){
+  if(join & ("data.frame" %in% class(companies))){
 
-    final_results_df = companies_df %>%
+    final_results_df = companies %>%
       dplyr::left_join(dplyr::bind_rows(final_df), by = "CompanyNumber")
 
   }else{
